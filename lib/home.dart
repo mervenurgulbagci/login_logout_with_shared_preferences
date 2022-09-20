3f9eab769147e8dcb93abf6fdfb646446cd8b8b2
mervenurgulbagci <mervenurgulbagci@gmail.com>
2022-09-20 19:26:55 +0300

    Logout added.

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'main.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,12 +11,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: SafeArea(child: Center(child: Text("Welcome User"))),
+        child: SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  Text("Welcome User"),
+                  SizedBox(height: 30),
+                  OutlinedButton.icon(
+                      onPressed:  () async {
+                        SharedPreferences pref = await SharedPreferences.getInstance();
+                        await pref.clear();
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(builder: (context) => LoginPage()),(route) => false);                    },
+                      icon: Icon(Icons.logout),
+                      label: Text("Logout"))
+                ],
+              ),
+
+        )),
       ),
     );
   }
